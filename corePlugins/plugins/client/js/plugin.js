94fea3b3a90429c0dd9358199878f183928f4051
Clemens Damke <admin@cortys.de>
2014-11-11 23:34:10 +0100

    moved list data to ng logic.
    still prototype stuff. has to be restructured later on.

--- corePlugins/plugins/client/js/plugin.js
+++ corePlugins/plugins/client/js/plugin.js
@@ -3,5 +3,43 @@
 
 	var plugin = angular.module("plugin", ["core"]);
 
-	
+	plugin.directive("pluginList", pluginList);
+
+	pluginList.$inject = [];
+
+	function pluginList() {
+
+		function Controller() {
+			this._active = null;
+		}
+
+		Controller.prototype = {
+			entries: [
+				{
+					name: "Hello World",
+					instances: 5
+				}, {
+					name: "Foo Bar",
+					instances: 3
+				}, {
+					name: "Baz Boo",
+					instances: 1
+				}
+			],
+			activate: function(plugin) {
+				this._active = plugin;
+			},
+			isActive: function(plugin) {
+				return plugin === this._active;
+			}
+		};
+
+		return {
+			restrict: "E",
+			controller: Controller,
+			controllerAs: "plugins",
+			transclude: true,
+			template: "<ng-transclude></ng-transclude>"
+		};
+	}
 }());
