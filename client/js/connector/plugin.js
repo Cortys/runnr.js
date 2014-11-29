bf61121f0ed2c06d8285a66ee1cb68e50e706639
Clemens Damke <admin@cortys.de>
2014-11-29 18:02:02 +0100

    Revert "Revert "added annotations""
    kek.

--- client/js/connector/plugin.js
+++ client/js/connector/plugin.js
@@ -40,6 +40,7 @@ var connector = (function() {
 			if(!t.connected && data.type == "handshake" && data.application == "runnr" && data.id == t.send.id) {
 				t.connected = true;
 				console.log("connected");
+				// TODO: Complete plugin receive handler
 				return;
 			}
 		}
