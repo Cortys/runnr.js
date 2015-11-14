9d8f17205b78fc7bb81023e24d5d0d2ad71f08c8
Clemens Damke <admin@cortys.de>
2015-11-14 21:21:12 +0100

    added event connector skeleton

---
+++ src/core/runners/sandbox/api/events/controller/connector/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const connector = {
+	__proto__: null,
+
+	addListener(object, event, api) {
+
+	},
+
+	removeListener(object, event, api) {
+
+	},
+
+	listeners(object, event, api) {
+
+	}
+};
+
+module.exports = connector;
