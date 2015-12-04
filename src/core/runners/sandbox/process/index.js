91367bf5aa2757e49ba0f70887c9f248014802c7
Clemens Damke <admin@cortys.de>
2015-12-04 18:07:12 +0100

    reorganized runner processes Runner class in the fs

--- src/core/runners/sandbox/process/index.js
+++ src/core/runners/sandbox/process/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
 const connector = require("./connector");
-const Runner = require("./Runner");
+const Runner = require("./runner/Runner");
 
 connector.register("nodes", new Runner(connector.master.route("runner", "graph")));
