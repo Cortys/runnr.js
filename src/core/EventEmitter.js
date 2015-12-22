9b6b71547d7e83eaada9e57c839b7d82bd46a6f3
Clemens Damke <admin@cortys.de>
2015-12-22 20:03:48 +0100

    hide _maxListeners property

--- src/core/EventEmitter.js
+++ src/core/EventEmitter.js
@@ -16,6 +16,12 @@ class EventEmitter extends require("events") {
 				configurable: false,
 				value: this._eventsCount // eslint-disable-line no-underscore-dangle
 			},
+			_maxListeners: {
+				writable: true,
+				enumerable: false,
+				configurable: false,
+				value: this._maxListeners // eslint-disable-line no-underscore-dangle
+			},
 			domain: {
 				writable: true,
 				enumerable: false,
