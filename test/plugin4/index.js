8cfe6f981e6c539012c360b7cd871133e2b621a5
Clemens Damke <admin@cortys.de>
2015-12-25 00:48:52 +0100

    updated test plugin 4

--- test/plugin4/index.js
+++ test/plugin4/index.js
@@ -1,21 +1,3 @@
 "use strict";
 
-/**package
-{
-	"name": "test-plugin-4",
-	"displayName": "Test Plugin 4",
-	"version": "0.1.0",
-	"author": "Clemens Damke",
-	"ports": {
-		"in": {
-			"input": {}
-		},
-		"out": {}
-	},
-	"dependencies": {
-		"owe-core": "latest"
-	}
-}
-**/
-
 console.log("kek");
