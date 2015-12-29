795dd29b204ba324590f5ac569c4fe83949f1a53
Clemens Damke <admin@cortys.de>
2015-12-30 00:56:20 +0100

    remove invalid runner graph edges after plugin updates

--- src/core/plugins/manage/update/index.js
+++ src/core/plugins/manage/update/index.js
@@ -2,6 +2,7 @@
 
 const owe = require("owe.js");
 
+const validateEdge = require("../../../graph/helpers/validateEdge")
 const manager = require("../manager");
 
 function update(plugin) {
@@ -10,7 +11,23 @@ function update(plugin) {
 
 	if(plugin.source === "localFile" || plugin.source === "localDirectory")
 		return plugin.deactivateDependents()
-			.then(() => sources.local(plugin));
+			.then(() => sources.local(plugin))
+			.then(() => {
+				for(const node of plugin.dependentNodes) {
+					const edges = node.edges;
+					const validate = edge => {
+						try {
+							validateEdge(edge);
+						}
+						catch(err) {
+							edge.delete();
+						}
+					};
+
+					edges.in.forEach(validate);
+					edges.out.forEach(validate);
+				}
+			});
 
 	return Promise.reject(new owe.exposed.Error("Invalid update source."));
 }
