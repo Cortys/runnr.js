845acb42a48dc57c337bf6c2ad2765bb99d572b0
Clemens Damke <admin@cortys.de>
2016-01-18 21:12:21 +0100

    restructured/cleaned up plugin update
    deactivateDeps & removing invalid edges are steps that are required for
    all types of update sources, so they have been moved out of the „local“
    update code branch.

--- src/core/plugins/manage/update/index.js
+++ src/core/plugins/manage/update/index.js
@@ -2,34 +2,38 @@
 
 const owe = require("owe.js");
 
-const validateEdge = require("../../../graph/helpers/validateEdge")
+const validateEdge = require("../../../graph/helpers/validateEdge");
 const manager = require("../manager");
 
 function update(plugin) {
 	if(plugin.source == null)
 		return Promise.reject(new owe.exposed.Error("This plugin has no update source."));
 
+	let updater;
+
 	if(plugin.source === "localFile" || plugin.source === "localDirectory")
-		return plugin.deactivateDependents()
-			.then(() => sources.local(plugin))
-			.then(() => {
-				for(const node of plugin.dependentNodes) {
-					const edges = node.edges;
-					const validate = edge => {
-						try {
-							validateEdge(edge);
-						}
-						catch(err) {
-							edge.delete();
-						}
-					};
+		updater = sources.local;
+	else
+		return Promise.reject(new owe.exposed.Error("Invalid update source."));
 
-					edges.in.forEach(validate);
-					edges.out.forEach(validate);
-				}
-			});
+	return plugin.deactivateDependents()
+		.then(() => updater(plugin))
+		.then(() => {
+			for(const node of plugin.dependentNodes) {
+				const edges = node.edges;
+				const validate = edge => {
+					try {
+						validateEdge(edge);
+					}
+					catch(err) {
+						edge.delete();
+					}
+				};
 
-	return Promise.reject(new owe.exposed.Error("Invalid update source."));
+				edges.in.forEach(validate);
+				edges.out.forEach(validate);
+			}
+		});
 }
 
 const sources = {
