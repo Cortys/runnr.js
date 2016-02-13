75ed29af8ca9334dbf46ed3d7d507d0bdc9ce3bf
Clemens Damke <admin@cortys.de>
2016-02-13 18:55:16 +0100

    config.fromPlugins resolves using nodes module algorithm instead of path concat
    preparation for the planned fully npm based plugin installation rewrite.

--- src/core/config.js
+++ src/core/config.js
@@ -1,18 +1,29 @@
 "use strict";
 
 const path = require("path");
+const resolve = require("resolve");
 
 const home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+const userData = path.join(home, ".runnr");
+const pluginData = path.join(userData, "plugins");
 
 module.exports = {
 	home,
-	userData: path.join(home, ".runnr"),
+	userData,
+	pluginData,
 
 	fromUserData() {
-		return path.resolve(this.userData, ...arguments);
+		return path.resolve(userData, ...arguments);
 	},
 
 	fromPlugins() {
-		return this.fromUserData("plugins", ...arguments);
+		return new Promise((accept, reject) => resolve(path.resolve(...arguments), {
+			basedir: pluginData
+		}, (err, result) => {
+			if(err)
+				return reject(err);
+
+			accept(result);
+		}));
 	}
 };
