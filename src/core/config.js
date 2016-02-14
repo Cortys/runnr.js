4110e3b8800d07bf9bfe8a04feb7e9f2fbff7ab5
Clemens Damke <admin@cortys.de>
2016-02-14 18:29:18 +0100

    ensure~/.runner/plugins/node_modules exist
    plugins will be installed into node_modules to enable a completely npm
    based plugin installation implementation.
    
    (npm install w/o package.json only works if node_modules dir is present
    in cwd)

--- src/core/config.js
+++ src/core/config.js
@@ -1,11 +1,16 @@
 "use strict";
 
 const path = require("path");
+const fs = require("fs-extra-promise");
 const resolve = require("resolve");
 
 const home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 const userData = path.join(home, ".runnr");
 const pluginData = path.join(userData, "plugins");
+
+// Synchronously ensure that  ~/.runnr/plugins/node_modules exist,
+// since config is loaded during init this is acceptable:
+fs.mkdirsSync(path.join(pluginData, "node_modules"));
 
 module.exports = {
 	home,
