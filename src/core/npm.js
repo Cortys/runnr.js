389ff41127d78bab83beac72f6bad0dc89b5853b
Clemens Damke <admin@cortys.de>
2016-02-14 19:09:17 +0100

    basic npm abstraction
    added simple api to execute npm commands in ~/.runnr/plugins

---
+++ src/core/npm.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const childProcess = require("child_process");
+const owe = require("owe.js");
+
+const npmCli = require.resolve("npm/bin/npm-cli");
+
+const config = require("./config");
+
+function npm() {
+	return new Promise((resolve, reject) => {
+		childProcess.fork(npmCli, Array.from(arguments), {
+			cwd: config.pluginData,
+			silent: true,
+			execArgv: []
+		}).once("exit", code => (code ? reject : resolve)(code));
+	});
+}
+
+module.exports = {
+	install(plugin) {
+		return npm("install", plugin, "--production", "--no-progress").catch(() => {
+			throw new owe.exposed.Error("Plugin could not be installed.");
+		});
+	},
+
+	uninstall(plugin) {
+		return npm("uninstall", plugin).catch(() => {
+			throw new owe.exposed.Error("Plugin could not be uninstalled.");
+		});
+	},
+
+	dedupe() {
+		return npm("dedupe").catch(() => {
+			throw new owe.exposed.Error("Plugin dependencies could not be deduplicated.");
+		});
+	}
+};
