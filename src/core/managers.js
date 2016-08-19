4b1bbf4c3d1365eca730b83ddada6f41068e6da1
Clemens Damke <admin@cortys.de>
2016-08-19 16:54:45 +0200

    updated stageManager
    now accepts nonCriticalStages = stages, that no not cause all jobs to
    fail if one job fails in them.

--- src/core/managers.js
+++ src/core/managers.js
@@ -2,5 +2,8 @@
 
 module.exports = {
 	taskManager: require("./helpers/taskManager")(),
-	stageManager: require("./helpers/stageManager")(["setMetadata", "assignGraph", "activateRunner"])
+	stageManager: require("./helpers/stageManager")({
+		stages: ["setMetadata", "assignGraph", "validatePlugin", "activateRunner"],
+		nonCriticalStages: ["validatePlugin", "activateRunner"]
+	})
 };
