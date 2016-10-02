e468346a65381287dfb1772fca77e9ffc3507245
Clemens Damke <admin@cortys.de>
2016-10-02 19:54:12 +0200

    reorganized plugins/manage/update

--- src/core/plugins/manage/update/index.js
+++ src/core/plugins/manage/update/index.js
@@ -8,18 +8,19 @@ function update(plugin) {
 	if(plugin.source == null)
 		return Promise.reject(new owe.exposed.Error("This plugin has no update source."));
 
-	let updater;
+	if(plugin.source in sources)
+		return sources[plugin.source](plugin);
 
-	if(plugin.source === "local")
-		updater = sources.local;
-	else
-		return Promise.reject(new owe.exposed.Error("Invalid update source."));
-
-	return updater(plugin);
+	return Promise.reject(new owe.exposed.Error("Invalid update source."));
 }
 
 const sources = {
-	local: require("./local")
+	__proto__: null,
+
+	local: require("./local"),
+	custom: () => {
+		throw new owe.exposed.Error("Custom plugins do not have update sources.");
+	}
 };
 
 module.exports = manager.taskify(update, plugin => plugin, "update");
