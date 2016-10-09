8296faa035bff75f42093e078b8637e743eebc79
Clemens Damke <admin@cortys.de>
2016-10-09 21:40:42 +0200

    added execution implementation for graph plugin nodes
    BIG MILESTONE: This makes it possible to develop a truly modular plugin
    infrastructure for runnr.

--- src/core/plugins/graph/executor/graph.js
+++ src/core/plugins/graph/executor/graph.js
@@ -1,7 +1,17 @@
 "use strict";
 
-function execute() {
+const { graph } = require("../../graph");
 
+function execute(node) {
+	const io = {
+		in: {},
+		out: {}
+	};
+
+	Object.keys(node.ports.in, name => io.in[name] = node.ports.in[name].readable);
+	Object.keys(node.ports.out, name => io.out[name] = node.ports.out[name].writable);
+
+	graph.createExecutor(node.plugin.graph, io);
 }
 
 module.exports = execute;
