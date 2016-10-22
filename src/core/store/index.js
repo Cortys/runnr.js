23a84218154f70a1dc6141d8c9a43fc4e941c1e0
Clemens Damke <admin@cortys.de>
2016-10-22 16:07:22 +0200

    fixed bug that prevented PluginNodes from loading
    The new Promise based Loki API settles store.loaded a few event loop turns later. This uncovered a race condition.
    
    PluginNodes were trying to get their Plugin while the DB was not done loading.
    
    To make startup more robust, the store.loaded lock is now part of the stageManager and blocks all non-basic startup work (basic = setting ids and names etc.) before the database is loaded.

--- src/core/store/index.js
+++ src/core/store/index.js
@@ -3,35 +3,45 @@
 const config = require("../config");
 const Loki = require("lokijs");
 
-const store = module.exports = new Loki(config.fromUserData("store.json"));
+const { stageManager } = require("../managers");
+
+const loaded = require("../helpers/generateLock")();
+
+const store = new Loki(config.fromUserData("store.json"));
 
 Object.assign(store, {
-	loaded: new Promise(resolve => {
-		setImmediate(() => {
-			const plugin = require("../plugins/plugin");
-			const Runner = require("../runners/Runner");
-
-			resolve(store.initializePersistence({
-				autoload: true,
-				autosave: true,
-				inflate: {
-					plugins: {
-						inflate: src => {
-							const instance = plugin.instanciate(src);
-
-							instance.assign(src);
-
-							return instance;
-						}
-					},
-					runners: {
-						proto: Runner,
-						inflate: (src, dst) => dst.assign(src)
-					}
-				}
-			}));
-		});
-	}).then(() => store),
+	loaded: loaded.then(() => store),
 
 	stop: () => store.close()
 });
+
+// Add loaded lock to stageManager to allow only basic Plugin & Runner assignments before the database is loaded.
+// This is necessary because some non-basic assignments use the database.
+stageManager({
+	initializeDatabase: () => loaded
+});
+
+module.exports = store;
+
+const plugin = require("../plugins/plugin");
+const Runner = require("../runners/Runner");
+
+store.initializePersistence({
+	autoload: true,
+	autosave: true,
+	inflate: {
+		plugins: {
+			inflate: src => {
+				const instance = plugin.instanciate(src);
+
+				instance.assign(src);
+
+				return instance;
+			}
+		},
+		runners: {
+			proto: Runner,
+			inflate: (src, dst) => dst.assign(src)
+		}
+	}
+}).then(loaded.unlock);
