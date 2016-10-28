59f5244a596e97e21a8c1e1cf055057dad05a4aa
Clemens Damke <admin@cortys.de>
2016-10-28 18:03:28 +0200

    fixed stupid bug
    function signatures...

--- src/core/plugins/graph/executor/graph.js
+++ src/core/plugins/graph/executor/graph.js
@@ -8,8 +8,8 @@ function execute(node) {
 		out: {}
 	};
 
-	Object.keys(node.ports.in, name => io.in[name] = node.ports.in[name].readable);
-	Object.keys(node.ports.out, name => io.out[name] = node.ports.out[name].writable);
+	Object.keys(node.ports.in).forEach(name => io.in[name] = node.ports.in[name].readable);
+	Object.keys(node.ports.out).forEach(name => io.out[name] = node.ports.out[name].writable);
 
 	graph.createExecutor(node.plugin.graph, io);
 }
