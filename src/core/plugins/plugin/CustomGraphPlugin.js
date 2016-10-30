99f92cfeb61fbce044b6481ac300b298991a73bf
Clemens Damke <admin@cortys.de>
2016-10-30 18:56:21 +0100

    fix: custom plugin graphs were not persisted

--- src/core/plugins/plugin/CustomGraphPlugin.js
+++ src/core/plugins/plugin/CustomGraphPlugin.js
@@ -6,10 +6,9 @@ const internalize = require("../../helpers/internalize");
 
 const abstract = require("./abstract");
 
-class GraphPlugin extends mixins(abstract.GraphPlugin) {
+class CustomGraphPlugin extends mixins(abstract.GraphPlugin) {
 	constructor() {
 		super(...arguments);
-		internalize(this, ["graph"]);
 
 		this[abstract.Plugin.exposed].publicRoutes.add("graph");
 	}
@@ -17,9 +16,14 @@ class GraphPlugin extends mixins(abstract.GraphPlugin) {
 	assign(preset) {
 		return super.assign(preset, {
 			writableGraph: true,
-			setDerivedData: () => this.graph.assign(preset.graph)
+			setDerivedData: () => {
+				// Internalize graph after setMetadata stage because Plugin removes all Object.keys on assign:
+				internalize(this, ["graph"]);
+
+				this.graph.assign(preset.graph);
+			}
 		});
 	}
 }
 
-module.exports = GraphPlugin;
+module.exports = CustomGraphPlugin;
